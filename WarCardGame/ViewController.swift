6929308383f584fd75a8d2ebd9a90dd92785a3ca
Matt Rutledge <mattrutledge@believegroup.com>
2016-12-21 12:30:41 -0700

    Initial commit of repo
    test iOS project that is a mini card game

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Matt Rutledge on 12/21/16.
+//  Copyright © 2016 Believe Kids. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
