b6c393c73cab2225ba17c414c9274339dec2552b
Matt Rutledge <mattrutledge@believegroup.com>
2016-12-21 13:24:24 -0700

    Add new IBOutlets for the card image views

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftCardView: UIImageView!
+    @IBOutlet weak var rightCardView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
