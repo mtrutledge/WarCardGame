5e285d35052ab75c146cc6636ee47905f62262e8
Matt Rutledge <mattrutledge@believegroup.com>
2016-12-21 13:35:14 -0700

    Adjust layout and hook up outlets
    Adjusted layout of a couple elements. Hooke up more elements and added
    functions to set the player score label and cpu score label

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -12,17 +12,31 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var leftCardView: UIImageView!
     @IBOutlet weak var rightCardView: UIImageView!
+    @IBOutlet weak var playerScoreLabel: UILabel!
+    @IBOutlet weak var cpuScoreLabel: UILabel!
+    
+    func viewInit() {
+        updatePlayerScore(score: 0)
+        updateCpuScore(score: 0)
+    }
+    
+    func updatePlayerScore(score:Int) {
+        playerScoreLabel.text = String(score)
+    }
+
+    func updateCpuScore(score:Int) {
+        cpuScoreLabel.text = String(score)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        viewInit()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
